4d350fc1a4ff62ac8124322e8d65504dc9da97e1
reggie_anim reggie_anim <reggieanim@gmail.com>
2020-07-24 18:58:41 +0000

    Updates

---
+++ precache-manifest.ef08f7a014bb15482d2ad332aab93537.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11993678062cfa3e37fb5b1eb242fa8",
+    "url": "/champ/index.html"
+  },
+  {
+    "revision": "67bfa7a9ef366f084428",
+    "url": "/champ/static/css/2.85c120cf.chunk.css"
+  },
+  {
+    "revision": "2b45ff15c10c447d0868",
+    "url": "/champ/static/css/main.1cdb07bd.chunk.css"
+  },
+  {
+    "revision": "67bfa7a9ef366f084428",
+    "url": "/champ/static/js/2.c3095232.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/champ/static/js/2.c3095232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b45ff15c10c447d0868",
+    "url": "/champ/static/js/main.36237a6b.chunk.js"
+  },
+  {
+    "revision": "ccdca01f1393facd94c0",
+    "url": "/champ/static/js/runtime-main.c5b1f87c.js"
+  },
+  {
+    "revision": "034229170eceddca9e300e8d2a4c8208",
+    "url": "/champ/static/media/FiraSans-Light.03422917.ttf"
+  },
+  {
+    "revision": "cd10747922b6d60d085bda79653c42d5",
+    "url": "/champ/static/media/RBNo2Light-Light.cd107479.ttf"
+  }
+]);
